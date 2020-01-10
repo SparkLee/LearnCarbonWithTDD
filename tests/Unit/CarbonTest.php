ed306085dd3161e4b65546f6af424993a16f14e4
SparkLee <liweijsj@163.com>
2020-01-10 15:52:59 +0800

    ignore .idea

--- tests/Unit/CarbonTest.php
+++ tests/Unit/CarbonTest.php
@@ -5,6 +5,9 @@ use Tests\TestCase;
 
 class CarbonTest extends TestCase
 {
+    /**
+     * 两个时间相差的分钟数
+     */
     public function test_diffInMinutes()
     {
         $result = Carbon::parse('2020-01-10 10:01:00')->diffInMinutes('2020-01-10 15:29:59');
